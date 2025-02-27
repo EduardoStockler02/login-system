512f87dca6d00cb8a29c4e123deb3ca74ce0ae4a
Seu Nome <seuemail@exemplo.com>
2025-02-27 10:22:18 -0300

    create the js

---
+++ main.js
@@ -0,0 +1,5 @@
+let email = document.getElementById('email');
+let name = document.getElementById('username');
+let password = document.getElementById('password')
+let password_confirm = document.getElementById('password-confirm')
+
